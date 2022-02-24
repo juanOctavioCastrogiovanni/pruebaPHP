7c25290c60fdd4d9eaf3d58ff95c5e63b54d27bd
JuanOctavioCastrogiovanni <jcastrogiovanni@est.frba.utn.edu.ar>
2022-02-24 15:09:43 -0300

    24-02 15:09

---
+++ db/blogup.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-02-2022 a las 17:34:28
+-- Versión del servidor: 10.1.34-MariaDB
+-- Versión de PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `blogup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `archivos`
+--
+
+CREATE TABLE `archivos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `id_blogs` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(45) NOT NULL,
+  `cuerpo` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_docentes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `catedras`
+--
+
+CREATE TABLE `catedras` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `id_docentes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docentes`
+--
+
+CREATE TABLE `docentes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `pass` text NOT NULL,
+  `imagen` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD KEY `relationship3_idx` (`id_blogs`);
+
+--
+-- Indices de la tabla `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `relationship2_idx` (`id_docentes`);
+
+--
+-- Indices de la tabla `catedras`
+--
+ALTER TABLE `catedras`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `relationship1_idx` (`id_docentes`);
+
+--
+-- Indices de la tabla `docentes`
+--
+ALTER TABLE `docentes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `catedras`
+--
+ALTER TABLE `catedras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `docentes`
+--
+ALTER TABLE `docentes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  ADD CONSTRAINT `relationship3` FOREIGN KEY (`id_blogs`) REFERENCES `blogs` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `blogs`
+--
+ALTER TABLE `blogs`
+  ADD CONSTRAINT `relationship2` FOREIGN KEY (`id_docentes`) REFERENCES `docentes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `catedras`
+--
+ALTER TABLE `catedras`
+  ADD CONSTRAINT `relationship1` FOREIGN KEY (`id_docentes`) REFERENCES `docentes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include("./header.php");
+?>
+<div class='cuerpo'>
+    <div class="panel">
+        <div>
+            <h1>Listado de blogs</h1>
+            <div class="card">
+                <img src="./updates/docentes/perfil1.jpg">
+                <h2>Titulo del blog del profe</h2>
+                <h4>fecha</h4>
+                <p>autor</p>
+            </div>
+            <div class="card">
+                <img src="./updates/docentes/perfil1.jpg">
+                <h2>Titulo del blog del profe</h2>
+                <h4>fecha</h4>
+                <p>autor</p>
+            </div>
+            <div class="card">
+                <img src="./updates/docentes/perfil1.jpg">
+                <h2>Titulo del blog del profe</h2>
+                <h4>fecha</h4>
+                <p>autor</p>
+            </div>
+            <div class="card">
+                <img src="./updates/docentes/perfil1.jpg">
+                <h2>Titulo del blog del profe</h2>
+                <h4>fecha</h4>
+                <p>autor</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("./footer.php");
+?>
